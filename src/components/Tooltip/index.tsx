1af9d8b758197dc59ede49efab7fd13eae25d15f
Jonathan Thaw <hi@jthaw.me>
2020-04-27 08:36:45 +0100

    allowed click anywhere to close tooltip

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback, useRef } from "react";
 
 import useMeasure from "react-use-measure";
 
@@ -13,23 +13,55 @@ interface TooltipProps {
 
 const Tooltip: React.FC<TooltipProps> = ({ children }: TooltipProps) => {
   const [ref, bounds] = useMeasure();
+  const elRef = useRef<HTMLDivElement | null>(null);
+
   const isRight = useMemo(() => {
     return window.innerWidth < bounds.x + 250;
   }, [bounds.x]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [transitioning, setTransitioning] = useState<boolean>(false);
 
+  const documentClickAway = useCallback((e) => {
+    if (elRef.current !== null) {
+      if (e.target !== elRef.current && !elRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    }
+  }, []);
+
+  const onBeforeExpand = useCallback(() => {
+    setTransitioning(true);
+
+    document.removeEventListener("click", documentClickAway);
+  }, []);
+
+  const onAfterExpand = useCallback((state: boolean) => {
+    setTransitioning(false);
+
+    if (state) {
+      document.addEventListener("click", documentClickAway);
+    }
+  }, []);
+
+  const onSetRef = useCallback(
+    (el) => {
+      ref(el);
+      elRef.current = el;
+    },
+    [ref],
+  );
+
   return (
     <div
-      ref={ref}
+      ref={onSetRef}
       className={`${styles.wrapper} ${isRight ? styles.right : ""}`}
     >
       <ExpandDiv
         initialClassName={styles.initial}
         expandedClassName={styles.expanded}
         expand={isOpen}
-        onBefore={(): void => setTransitioning(true)}
-        onAfter={(): void => setTransitioning(false)}
+        onBefore={onBeforeExpand}
+        onAfter={onAfterExpand}
         className={styles.expandElement}
         absolute
       >
